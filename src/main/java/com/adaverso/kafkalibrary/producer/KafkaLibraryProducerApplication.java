aff932458623f28d454b11f1bff732e100aada3b
adail <adail.dux@gmail.com>
2021-11-09 12:06:26 -0300

    Adds initial spring boot setup

---
+++ src/main/java/com/adaverso/kafkalibrary/producer/KafkaLibraryProducerApplication.java
@@ -0,0 +1,13 @@
+package com.adaverso.kafkalibrary.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLibraryProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLibraryProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adaverso/kafkalibrary/producer/KafkaLibraryProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaverso.kafkalibrary.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLibraryProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
